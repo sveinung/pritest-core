d23be0eb991585c39021b18a37595c443a982cb6
Sveinung Dalatun <sdalatun@gmail.com>
2010-10-15 15:03:09 +0200

    First commit

---
+++ src/test/java/restapi/model/MeasureTest.java
@@ -0,0 +1,18 @@
+package restapi.model;
+
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import restapi.model.Measure;
+
+public class MeasureTest {
+
+    @Test
+    public void shouldSupportName() {
+        Measure measure = new Measure("abc");
+        assertThat(measure.getName(), equalTo("abc"));
+    }
+}
