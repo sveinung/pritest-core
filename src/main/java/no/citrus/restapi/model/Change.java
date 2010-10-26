fb689ce64b8df17f88c8019b3cfcbbf6b6635da0
Øyvind Voldsund <oyvindvol@gmail.com>
2010-10-26 12:45:30 +0200

    mod. Change.

--- src/main/java/no/citrus/restapi/model/Change.java
+++ src/main/java/no/citrus/restapi/model/Change.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement (name = "payload")
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Change {
 	public String after;
