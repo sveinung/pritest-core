bff14d3effd964867f32e105a4b8be8f885016a7
Sveinung Dalatun <sdalatun@gmail.com>
2010-11-10 11:24:15 +0100

    Added setter to MeasureList

--- src/main/java/no/citrus/restapi/model/Measure.java
+++ src/main/java/no/citrus/restapi/model/Measure.java
@@ -8,9 +8,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Measure {
 	public String source;
--- src/main/java/no/citrus/restapi/model/MeasureList.java
+++ src/main/java/no/citrus/restapi/model/MeasureList.java
@@ -2,21 +2,28 @@ package no.citrus.restapi.model;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="measures")
-public class MeasureList<T>{
-    protected List<T> list;
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class MeasureList{
+    protected List<Measure> list;
 
     public MeasureList(){}
 
-    public MeasureList(List<T> list){
+    public MeasureList(List<Measure> list){
         this.list=list;
     }
 
     @XmlElement(name="measure")
-    public List<T> getList(){
+    public List<Measure> getList(){
         return list;
+    }
+    
+    public void setList(List<Measure> list) {
+    	this.list = list;
     }
 }
